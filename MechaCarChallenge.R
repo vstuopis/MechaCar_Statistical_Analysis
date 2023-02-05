9ced5582461c6dbfe2b8f74d686982bcbac99e45
Vytas Stuopis <113248280+vstuopis@users.noreply.github.com>
2023-02-05 15:40:54 -0500

    Adding data on t-tests on suspension Coils.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,15 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 suspencoil_df <- read.csv(file='Suspension_Coil.csv')
 total_summary <- suspencoil_df %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 lot_summary <- suspencoil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+t.test(log10(suspencoil_df$PSI),mu= 150)
+
+lot_one <- subset(suspencoil_df, Manufacturing_Lot=='Lot1', drop = TRUE)
+t.test(log10(lot_one$PSI),mu= 150)
+
+lot_two <- subset(suspencoil_df, Manufacturing_Lot=='Lot2', drop = TRUE)
+t.test(log10(lot_two$PSI),mu= 150)
+
+lot_three <- subset(suspencoil_df, Manufacturing_Lot=='Lot3', drop = TRUE)
+t.test(log10(lot_three$PSI),mu= 150)
+
