59ec96485558a825905c730a6a356519d6bc8c88
Vytas Stuopis <vytas.stuopis@gmail.com>
2023-02-05 13:39:16 -0500

    Adding screenshots of data an updated R code with Suspension Coil data.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,3 +2,7 @@ library(dplyr)
 mpg_df <- read.csv(file='MechaCar_mpg.csv')
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_df)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_df))
+
+suspencoil_df <- read.csv(file='Suspension_Coil.csv')
+total_summary <- suspencoil_df %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_summary <- suspencoil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
